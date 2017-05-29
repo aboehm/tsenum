04003cc8ccd32e4e70635e17e17c8bd67389758c
Alexander Böhm <alxndr.boehm@gmail.com>
2017-05-29 12:24:52 +0200

    set version to 0.2

--- tsenum/__init__.py
+++ tsenum/__init__.py
@@ -18,7 +18,7 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-version     = '0.1'
+version     = '0.2'
 prog        = 'tsenum'
 author      = 'Alexander Böhm'
 author_mail = 'alxndr.boehm@gmail.com'
